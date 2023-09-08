11a9fcdce779c7bfc8600309e20fb802b1603308
Tetiana Dmytrash <tdmyt@softserveinc.com>
2023-09-08 21:21:17 +0300

    add website

---
+++ website/static/index.js
@@ -0,0 +1,10 @@
+//запрос на серверную часть
+
+function deleteNote(noteId) {
+  fetch("/delete-note", {
+    method: "POST",
+    body: JSON.stringify({ noteId: noteId }),
+  }).then((_res) => {
+    window.location.href = "/"; //reload window
+  });
+}
